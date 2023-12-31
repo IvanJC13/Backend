4c70ad28d592427758d0decbefaebbf65ed5d175
Edgar Ivan Juarez Cadena <edgarivanjuarezcadena40@gmail.com>
2023-12-31 11:05:33 -0600

    backend

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-import express  from "express"
-import cors from 'cors'
-//importamos la conexión a la DB
-import db from "./database/db.js"
-//importamos nuestro enrutador
-import blogRoutes from './routes/routes.js'
+import express from 'express';
+import cors from 'cors';
+import db from './database/db.js';
+import blogRoutes from './routes/routes.js';
 
-const app = express()
-const port = process.env.PORT || 8000;
-app.use(cors())
-app.use(express.json())
-app.use('/blogs', blogRoutes)
+const app = express();
 
+// Configuración CORS
+app.use(cors());
+
+// Configuración del parser JSON
+app.use(express.json());
+
+// Configuración de las rutas
+app.use('/blogs', blogRoutes);
+
+// Conexión a la base de datos
 try {
-    await db.authenticate()
-    console.log('Conexión exitosa a la DB')
+    await db.authenticate();
+    console.log('Conexión exitosa a la DB');
 } catch (error) {
-    console.log(`El error de conexión es: ${error}`)
+    console.log(`El error de conexión es: ${error}`);
 }
-
+const port = process.env.PORT || 8000;
 app.listen(port, () =>{
     console.log(`Esta corriendo en http://localhost:${port}`);
 });
